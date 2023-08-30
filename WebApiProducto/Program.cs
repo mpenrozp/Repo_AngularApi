ad73b7bec39748fc70bcc4192f997727c1aba915
mpenr <mpenr@SANTITO>
2023-08-30 00:07:53 -0400

    .

--- WebApiProducto/Program.cs
+++ WebApiProducto/Program.cs
@@ -5,10 +5,11 @@ using System.Text;
 
 using WebApiProducto.Services;
 using WebApiProducto.Interfaces;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 builder.Services.AddAuthentication(options =>
@@ -35,13 +36,21 @@ builder.Services.AddAuthentication(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add services to the container.
 builder.Services.AddScoped<IProductos, ServiceProducto>();
 builder.Services.AddScoped<IToken, ServiceToken>();
 builder.Services.AddHttpClient();
 
+
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+});
 var app = builder.Build();
+
+app.UseHttpLogging();
 
 app.UseCors( options => 
     options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
